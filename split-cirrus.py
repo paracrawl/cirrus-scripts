9eae9c90687571577881f049e9dce13c9ae0dbce
William Waites <ww@groovy.net>
2020-01-24 14:47:29 +0000

    preprocessing

---
+++ split-cirrus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+## Split appropriately for whole-node jobs on cirrus
+
+from sys import argv
+import fileinput
+
+nrows=72
+
+slice=0
+n=72
+ofp=None
+filebase=argv[1]
+for row in fileinput.input(filebase):
+    if n >= nrows:
+        if ofp is not None:
+            ofp.close()
+        slice += 1
+        ofp = open("%s.%d" % (filebase, slice), "w+")
+        n = 0
+    ofp.write(row)
+    n += 1
