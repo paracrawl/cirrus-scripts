6407a83d9f8352fb9fcaea83ac606f7d2ccc15ca
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-04-02 15:12:08 +0100

    Simplify 06.align and group per batch instead of per shard to work around massive /en/41/

--- 06.align.sh
+++ 06.align.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 ## create and submit the batches on csd3 for alignment
+set -euo pipefail
 
 . ./config.csd3
-export TASKS_PER_BATCH=4
+export TASKS_PER_BATCH=4 # Also used by 06.align.slurm, hence export
 
 collection=$1
 shift
 
 for lang in $*; do
-	if ! test -d ${DATA}/${collection}-batches/${lang}; then
-		ls -d ${DATA}/${collection}-shards/${lang}/*/* > ${DATA}/${collection}-batches/${lang}
+	if ! test -d ${DATA}/${collection}-batches/06.${lang}; then
+		for shard in $(ls -d ${DATA}/${collection}-shards/${lang}/*); do
+			join -j2 \
+				<(ls -d $shard/*) \
+				<(ls -d ${DATA}/${collection}-shards/en/$(basename $shard)/*)
+		done > ${DATA}/${collection}-batches/06.${lang}
 	fi
-	rm -f ${DATA}/${collection}-batches/06.${lang}
-	ln -s   ${DATA}/${collection}-batches/${lang} ${DATA}/${collection}-batches/06.${lang}
-	n=$((`< ${DATA}/${collection}-batches/06.${lang} wc -l`/${TASKS_PER_BATCH}))
+	n=$(( $(< ${DATA}/${collection}-batches/06.${lang} wc -l) / ${TASKS_PER_BATCH}))
 	sbatch --nice=600 -J align-${lang} -a 1-${n} ${SCRIPTS}/06.align.slurm ${lang} ${DATA}/${collection}-batches/06.${lang}
 done
