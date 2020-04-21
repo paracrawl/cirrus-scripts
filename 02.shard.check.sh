bcb5c7a0564ed9aa7ac2e779b4855c912e267686
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-04-21 22:45:21 +0100

    Add checking scripts

---
+++ 02.shard.check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+. ./config.csd3
+. ./functions.sh
+
+THREADS=${THREADS:-4}
+
+collection=$1
+shift
+
+function validate () {
+	local mime_cnt=$(gzip -cd $1/mime.gz | wc -l)
+	local source_cnt=$(gzip -cd $1/source.gz | wc -l)
+	local doc_cnt=$(gzip -cd $1/plain_text.gz | wc -l)
+	if test ! "$mime_cnt" -eq "$source_cnt" || test ! "$source_cnt" -eq "$doc_cnt"; then
+		echo $1
+	fi
+}
+
+export -f validate
+
+for lang in $@; do
+	ls -d $DATA/$collection-shards/$lang/*/* | parallel --line-buffer validate 
+done
+		
---
+++ 03.split-text.check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+. ./config.csd3
+. ./functions.sh
+
+THREADS=${THREADS:-4}
+
+collection=$1
+shift
+
+function validate () {
+	local docs_pt=$(gzip -cd $1/plain_text.gz | wc -l)
+	local docs_st=$(gzip -cd $1/sentences.gz | wc -l)
+	if test ! "$docs_pt" -eq "$docs_st"; then 
+		echo $1
+	fi
+}
+
+export -f validate
+
+for lang in $@; do
+	ls -d $DATA/$collection-shards/$lang/*/* | parallel --line-buffer validate 
+done
+		
