e6f18471fb2bf6ec5a5d0097e856e777fa95bb4e
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-04-21 22:27:47 +0100

    Add checks

--- 03.split-text.sh
+++ 03.split-text.sh
@@ -17,7 +17,8 @@ for lang in $*; do
 	job_list=$(make_job_list $batch_list sentences.gz)
 	if [ ! -z $job_list ]; then
 		echo Scheduling $job_list on $ARCH
-		confirm
-		sbatch --nice=300 -J split-${lang} -a $job_list 03.split-text.slurm $lang $batch_list
+		if confirm; then 
+			sbatch --nice=300 -J split-${lang} -a $job_list 03.split-text.slurm $lang $batch_list
+		fi
 	fi
 done
