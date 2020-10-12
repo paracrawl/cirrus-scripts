f21cf7b4dcc9e181bd23a0d78f2f1433df56e6e4
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-10-12 16:43:20 +0100

    Add external translation scripts for reference

---
+++ translate_zh_en.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+cd /rds/project/t2_vol4/rds-t2-cs119/cwang/nmt/home/bhaddow/experiments/paracrawl/chinese/mt
+
+$JIEBA -d ' ' \
+| python3 $BPE/apply_bpe.py \
+	-c prepare01/bpe.model.zh \
+| $MARIAN/marian-decoder \
+	-m run02/model.npz.best-translation.npz \
+	-v run02/vocab.{zh,en}.yml \
+	-w 3200 \
+	--optimize \
+	--normalize 1 \
+	--word-penalty 0 \
+	--beam-size 4 \
+	--mini-batch-words 300 \
+	--maxi-batch 2000 \
+	--maxi-batch-sort src \
+	"$@" \
+| perl -pe 's/@@ //g' \
+| python2 prepare01/deseg.py
+
