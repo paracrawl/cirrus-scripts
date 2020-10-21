b6fda0123ce52b861258adb89c04fd862edb6284
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-10-21 20:32:19 +0100

    Prevent possible Jieba race condition; use shortlist

--- translate_zh_en.sh
+++ translate_zh_en.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+
 cd /rds/project/t2_vol4/rds-t2-cs119/cwang/nmt/home/bhaddow/experiments/paracrawl/chinese/mt
 
-$JIEBA -d ' ' \
+# Give Jieba it's own tempdir to avoid race conditions when it's dumping its model.
+JIEBA_TMP=$(mktemp -d)
+
+TMPDIR=$JIEBA_TMP $JIEBA -d ' ' \
 | python3 $BPE/apply_bpe.py \
 	-c prepare01/bpe.model.zh \
 | $MARIAN/marian-decoder \
@@ -17,8 +21,11 @@ $JIEBA -d ' ' \
 	--mini-batch-words 3000 \
 	--maxi-batch 4000 \
 	--maxi-batch-sort src \
+	--shortlist /home/cs-vand1/src/cirrus-scripts/lex.zhen \
 	"$@" \
 | perl -pe 's/@@ //g' \
 |  $MOSES/scripts/recaser/detruecase.perl \
 |  $MOSES/scripts/tokenizer/detokenizer.perl -q -l en
+
+rm -fr $JIEBA_TMP
 
