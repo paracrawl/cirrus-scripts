8d8de919cefb538d3d7594dc2c9b5262c40798e9
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-11-20 18:14:39 +0000

    babysteps towards integrating clean-csd3 into this repository
    With the bonus of zh/ko support I need right now.

---
+++ 08.extract.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+. config.csd3
+. functions.sh
+
+collection=$1
+shift
+
+for lang in $*; do
+	schedule \
+		-J extract-$lang \
+		--cpus-per-task 1 \
+		--time 4:00:00 \
+		-e ${SLURM_LOGS}/08.extract-%A.err \
+		-o ${SLURM_LOGS}/08.extract-%A.out \
+		$SCRIPTS/08.extract $collection $lang
+done
