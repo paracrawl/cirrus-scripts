1371ebfa569977b8fd59f7d52e420d21c99409ec
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-01-04 11:39:10 +0000

    Add lib64 for cirrus

--- env/init.sh
+++ env/init.sh
@@ -12,8 +12,8 @@ export PATH="$PREFIX/bin:$GOPATH/bin:$PATH"
 export INCLUDE="$PREFIX/include${INCLUDE:+:$INCLUDE}"
 export LIB="$PREFIX/lib${LIB:+:$LIB}"
 export CPATH="$PREFIX/include${CPATH:+:$CPATH}"
-export LIBRARY_PATH="$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
-export LD_LIBRARY_PATH="$PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 # Note: this will make using KNL nodes a no-go
 export CFLAGS="-O3 -march=native -pipe"
