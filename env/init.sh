c57306cafbd713227a7a01905172045fb3476810
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-01-04 11:23:13 +0000

    break up init file and add limited support for Cirrus (coming full circle here)

--- env/init.sh
+++ env/init.sh
@@ -1,22 +1,11 @@
 PREFIX=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 
-module purge
+source $PREFIX/init.d/*.sh
 
 # Also explicitly puring these (except PATH) to get rid of
 # any user defined environment.
 #unset INCLUDE LIB CPATH LD_LIBRARY_PATH LIBRARY_PATH
 unset LD_LIBRARY_PATH
-
-module load \
-	python/3.8 \
-	perl-5.26.2-gcc-7.2.0-2ys4uai \
-	gcc/9 \
-	cmake \
-	intel/mkl/2020.2 \
-	openssl-1.0.2k-gcc-5.4.0-lonhprt \
-	binutils-2.31.1-gcc-5.4.0-uyyspmn
-
-# protobuf-3.4.0-gcc-5.4.0-zkpendv \
 
 export GOPATH="$PREFIX/go"
 export PATH="$PREFIX/bin:$GOPATH/bin:$PATH"
