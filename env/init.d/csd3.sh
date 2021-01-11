7756b094ada6074acb9db7f2715b1146f5f09fa1
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-01-11 10:32:10 +0000

    Make warc2text compile on CSD3.
    I don't know why but I was having issues with the Python/3.8 module stealing my include path priority. The lzma header files were found there instead of in the system include or the xz module include folders, while the so files that were being linked were taken from elsewhere. This caused some compilation issues as the header files and so files found were from different versions of lzma. By just manually symlinking the header and so files into our prefix it compiles as expected.

--- env/init.d/csd3.sh
+++ env/init.d/csd3.sh
@@ -4,10 +4,11 @@ if [[ "$(hostname -A)" =~ "hpc.cam.ac.uk" ]]; then
 	module purge && module load \
 		python/3.8 \
 		perl-5.26.2-gcc-7.2.0-2ys4uai \
-		gcc/9 \
 		cmake \
+		gcc/9 \
+		binutils-2.31.1-gcc-5.4.0-uyyspmn \
 		intel/mkl/2020.2 \
-		openssl-1.0.2k-gcc-5.4.0-lonhprt \
-		binutils-2.31.1-gcc-5.4.0-uyyspmn
+		xz-5.2.4-gcc-7.2.0-v7gjh3h \
+		openssl-1.0.2k-gcc-5.4.0-lonhprt
 fi
 
--- env/init.sh
+++ env/init.sh
@@ -5,6 +5,12 @@ PREFIX=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 #unset INCLUDE LIB CPATH LD_LIBRARY_PATH LIBRARY_PATH
 unset LD_LIBRARY_PATH
 
+# Next, load in the system specific modules
+for initfile in $PREFIX/init.d/*.sh; do
+	source $initfile
+done
+
+# Finally, add our environment to the front, so it has highest priority
 export GOPATH="$PREFIX/go"
 export PATH="$PREFIX/bin:$GOPATH/bin:$PATH"
 export INCLUDE="$PREFIX/include${INCLUDE:+:$INCLUDE}"
@@ -12,12 +18,9 @@ export LIB="$PREFIX/lib${LIB:+:$LIB}"
 export CPATH="$PREFIX/include${CPATH:+:$CPATH}"
 export LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
 export LD_LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export PKG_CONFIG_PATH="$PREFIX/lib64/pkgconfig:$PREFIX/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 
 # Note: this will make using KNL nodes a no-go
 export CFLAGS="-O3 -march=native -pipe"
 export CXXFLAGS="-O3 -march=native -pipe"
-
-for initfile in $PREFIX/init.d/*.sh; do
-	source $initfile
-done
 
