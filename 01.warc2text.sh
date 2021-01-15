9df1f6b17a4823e93033c38c2e3af931af39a9af
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-01-15 14:12:54 +0000

    Add confirm prompt, and log to output folder as well

--- 01.warc2text.sh
+++ 01.warc2text.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
 . ./env/init.sh
 . ./config.sh
 . ./functions.sh
@@ -33,14 +32,17 @@ for collection in $@; do
 	job_list=$(make_job_list $batch_list)
 	output_dir="${COLLECTIONS[$collection]}-text/"
 	if [ ! -z "$job_list" ]; then
-		schedule \
-			-J warc2text-${collection} \
-			-a $job_list \
-			--time 24:00:00 \
-			--cpus-per-task 1\
-			-e ${SLURM_LOGS}/01.warc2text-%A_%a.err \
-			-o ${SLURM_LOGS}/01.warc2text-%A_%a.out \
-			${SCRIPTS}/generic.slurm $batch_list \
-			${SCRIPTS}/01.warc2text $output_dir
+		prompt "Run $job_list warc2text?"
+		if confirm; then
+			schedule \
+				-J warc2text-${collection} \
+				-a $job_list \
+				--time 24:00:00 \
+				--cpus-per-task 1\
+				-e ${SLURM_LOGS}/01.warc2text-%A_%a.err \
+				-o ${SLURM_LOGS}/01.warc2text-%A_%a.out \
+				${SCRIPTS}/generic.slurm $batch_list \
+				${SCRIPTS}/01.warc2text $output_dir
+		fi
 	fi
 done
