276de8cd862f676ba3e33e5ab2994c8e1da29532
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-02-02 11:12:56 +0000

    Remove partial shards after combining them

--- 02.giashard.sh
+++ 02.giashard.sh
@@ -26,14 +26,15 @@ for language in $@; do
 	output_dir="${COLLECTIONS[$collection]}-shards/"
 	job_list=$(TASKS_PER_BATCH=$BATCHES_PER_TASK make_job_list $batch_list)
 
-	if ! $RETRY && [ -d "$output_dir" ]; then
+	if ! $RETRY && [ -d "$output_dir/${language}" ]; then
+		echo Skipping $language
 		continue
 	fi
 	
 	if [ ! -z "$job_list" ]; then
-		prompt "Run $job_list giashard for ${collection}?\n"
+		prompt "Run $job_list giashard for ${collection}/${language}?\n"
 		if confirm; then
-			job_id=$(schedule \
+			shard_job_id=$(schedule \
 				-J shard-${language}-${collection} \
 				-a $job_list \
 				--time 24:00:00 \
@@ -41,16 +42,25 @@ for language in $@; do
 				-e ${SLURM_LOGS}/02.shard-${language}-%A_%a.err \
 				-o ${SLURM_LOGS}/02.shard-${language}-%A_%a.out \
 				${SCRIPTS}/02.giashard $batch_list $language $output_dir)
-			echo $job_id
-			schedule \
+			echo $shard_job_id
+			merge_job_id=$(schedule \
 				-J merge-${language}-${collection} \
-				--dependency afterok:$job_id \
+				--dependency afterok:$shard_job_id \
 				-a 1-16 \
 				--time 12:00:00 \
 				--cpus-per-task 4 \
 				-e ${SLURM_LOGS}/02.merge-${language}-%A_%a.err \
 				-o ${SLURM_LOGS}/02.merge-${language}-%A_%a.out \
-				${SCRIPTS}/02.giamerge $job_list $language $output_dir
+				${SCRIPTS}/02.giamerge $job_list $language $output_dir)
+			echo $merge_job_id
+			schedule \
+				-J clean-${language}-${collection} \
+				--dependency afterok:$merge_job_id \
+				--time 12:00:00 \
+				--cpus-per-task 1 \
+				-e ${SLURM_LOGS}/02.clean-${language}-%A_%a.out \
+				-o ${SLURM_LOGS}/02.clean-${language}-%A_%a.out \
+				${SCRIPTS}/02.clean $job_list $language $output_dir
 		fi
 	fi
 done
