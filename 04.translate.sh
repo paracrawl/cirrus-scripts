ae51747efa6a1a4c5087556ddb2a0e32b0a8880a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-02-05 12:51:28 +0000

    Simplify translation step (todo: delete old files)

--- 04.translate.sh
+++ 04.translate.sh
@@ -3,10 +3,10 @@
 ## create and submit the batches on csd3 for translation
 set -euo pipefail
 
+. env/init.sh
 . config.sh
 . functions.sh
 . translate.sh
-export -f get_group_boundaries task
 
 collection=$1
 shift
@@ -15,12 +15,19 @@ for lang in $*; do
 	# Load in translation model config so we know MODEL_ARCH
 	eval model_${lang}_${TARGET_LANG} || (echo "No model for ${lang} -> ${TARGET_LANG}" 1>&2 ; exit 255)
 	batch_list=$(make_batch_list 04 $collection $lang sentences_${TARGET_LANG}.gz)
-	# ${EDITOR:-vim} $batch_list
 	job_list=$(make_job_list $batch_list)
 	if [ ! -z $job_list ]; then
-		prompt "Scheduling $job_list on $MODEL_ARCH\n"
+		prompt "Scheduling $job_list\n"
 		if confirm; then
-			schedule -J translate-${lang} -a $job_list 04.translate.${MODEL_ARCH}.slurm $lang $batch_list
+			schedule \
+				-J translate-${lang} \
+				-a $job_list \
+				--time 24:00:00 \
+				--cpus-per-task 4 \
+				-e $SLURM_LOGS/04.translate-${lang}-%A_%a.err \
+				-o $SLURM_LOGS/04.translate-${lang}-%A_%a.out \
+				$SCRIPTS/generic.slurm $batch_list \
+				$SCRIPTS/04.translate $lang
 		fi
 	fi
 done
