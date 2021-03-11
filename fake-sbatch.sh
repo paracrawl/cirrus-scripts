2493f241cec9397415fb73acde1dbd63e44d9eb6
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-03-11 10:45:08 +0000

    Update fake-sbatch.sh to be more useful

--- fake-sbatch.sh
+++ fake-sbatch.sh
@@ -4,16 +4,23 @@ set -euo pipefail
 export SLURM_TASKS_PER_NODE=1
 export SLURM_CPUS_ON_NODE=1
 export SLURM_ARRAY_TASK_ID=1
+export SLURM_ARRAY_JOB_ID=1
 
+echo "$@" >&2
 while [[ "$1" = -* ]]; do
 	if [[ "$1" == "--cpus-per-task" ]]; then
 		export SLURM_CPUS_PER_TASK=$2
 		export SLURM_CPUS_ON_NODE=$2
+		shift 2
 	elif [[ "$1" == "--ntasks" ]]; then
 		export SLURM_TASKS_PER_NODE=$2
+		shift 2
+	elif [[ "$1" == "--parsable" ]] || [[ "$1" == "--verbose" ]]; then
+		shift 1
+	else
+		shift 2
 	fi
-
-	shift 2
 done
+
 
 exec "$@"
