4e514f9ac5ee8bb4aaf5567d86babf1314dcdd79
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-03-16 17:20:30 +0000

    Update unclean corpus script

--- 07.unclean-corpus.sh
+++ 07.unclean-corpus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+. env/init.sh
 . config.sh
 . functions.sh
 
@@ -8,7 +9,14 @@ collection=$1
 shift
 
 for lang in $*; do
-	schedule -J corpus-$lang $SCRIPTS/07.unclean-corpus.slurm $collection $lang
-	# bash 07.unclean-corpus.slurm $collection $lang
+	schedule \
+		-J corpus-${lang%~*}-${collection} \
+		--time 24:00:00 \
+		-e ${SLURM_LOGS}/07.corpus-%A_%a.err \
+		-o ${SLURM_LOGS}/07.corpus-%A_%a.out \
+		${SCRIPTS}/07.unclean-corpus.slurm \
+		${COLLECTIONS[$collection]}-corpora/$collection-unclean.${TARGET_LANG%~*}-${lang%~*}.gz \
+		${COLLECTIONS[$collection]}-shards/${TARGET_LANG} \
+		${COLLECTIONS[$collection]}-batches/${lang}
 done
 
