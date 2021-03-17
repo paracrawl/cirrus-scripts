cea4ef39de35168d12ca6ef3bfe62fa88a5787ea
ZJaume <jzaragoza@prompsit.com>
2021-03-17 16:52:42 +0000

    Use correct data cleaning destination path

--- 11.reduce-filtered.sh
+++ 11.reduce-filtered.sh
@@ -41,7 +41,7 @@ for collection in $collections; do
 	batch_lists+=( $batch_list )
 done
 
-output_file="${DATA}/cleaning/${TARGET_LANG}-${lang}/${TARGET_LANG}-${lang}.${collection_hash}.filtered${BICLEANER_THRESHOLD/./}.gz"
+output_file="${DATA_CLEANING}/cleaning/${TARGET_LANG}-${lang}/${TARGET_LANG}-${lang}.${collection_hash}.filtered${BICLEANER_THRESHOLD/./}.gz"
 
 if [ ! -f $output_file ]; then
 	prompt "Scheduling 1-1 for combining $batch_count batches across ${#batch_lists[@]} collections\n"
