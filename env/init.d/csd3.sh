175032a71fcd52fd24b7e778263c3ca45d61942d
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-03-20 00:07:18 +0000

    Adding xz lib only confuses ld, as it compiles with headers for 5.2.2

--- env/init.d/csd3.sh
+++ env/init.d/csd3.sh
@@ -13,7 +13,6 @@ if [[ "$(hostname -A)" =~ "hpc.cam.ac.uk" ]]; then
 		binutils-2.31.1-gcc-5.4.0-uyyspmn `# Nick: newer binutils is better` \
 		intel/mkl/2020.2 \
 		cuda/10.2 `# Marian doesn't support 11 properly yet` \
-		xz-5.2.4-gcc-7.2.0-v7gjh3h `# liblzma` \
 		openssl-1.0.2k-gcc-5.4.0-lonhprt
 fi
 
