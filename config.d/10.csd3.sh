9ef3c4e298bbca7b6f8e2af8a9bd5c10bcf43482
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-05-31 14:36:08 +0100

    Remove merged collection for now

--- config.d/10.csd3.sh
+++ config.d/10.csd3.sh
@@ -39,7 +39,7 @@ if [[ "$(hostname -A)" =~ "hpc.cam.ac.uk" ]]; then
 		["cc-2018-30"]="$COLLECTION_ROOT/paracrawl/data/common_crawl/CC-MAIN-2018-30"
 		["cc-2019-18"]="$COLLECTION_ROOT/paracrawl/data/common_crawl/CC-MAIN-2019-18"
 		["cc-2019-35"]="$COLLECTION_ROOT/paracrawl/data/common_crawl/CC-MAIN-2019-35"
-		["merged"]="$COLLECTION_ROOT/paracrawl/data/merged/merged"
+		#["merged"]="$COLLECTION_ROOT/paracrawl/data/merged/merged"
 		["marta"]="$COLLECTION_ROOT/paracrawl/data/marta/marta"
 	)
 
