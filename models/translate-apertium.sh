da048489dee37331109a868af929e1edb0d08d70
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-06-16 17:00:18 +0100

    Use apertium script from Paracrawl agenda; add bisector because apertium is odd

--- models/translate-apertium.sh
+++ models/translate-apertium.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export MODEL=$1
 shift
 
-translate-apertium() {
+bad-translate-apertium() {
 	sed "s/\xc2\xad/ /g" \
 	| sed "s/\x00//g" \
 	| sed 's/&#10;//g' \
@@ -18,6 +18,16 @@ translate-apertium() {
 	| apertium -f html -u $MODEL \
 	| sed -r 's/<\/?p>//g' \
 	| recode html
+}
+
+translate-apertium() {
+	sed 's/</\&lt;/g' \
+	| sed 's/>/\&gt;/g' \
+	| sed 's/^/<p>/g' \
+	| sed 's/$/<\/p>/g' \
+	| apertium -f html -u $MODEL \
+	| sed -r 's/<\/?p>//g' \
+	| $PERL -CS -MHTML::Entities -pe 'decode_entities($_);'
 }
 
 ceil () {
@@ -35,5 +45,5 @@ fi
 export -f translate-apertium
 
 # No foldfilter? No, don't know who sensitive apertium is to that kind of force
-parallel --halt 2 -j $THREADS --pipe -k -l $RECSIZE translate-apertium "$@"
+parallel --halt 2 -j $THREADS --pipe -k -l $RECSIZE $SCRIPTS/bisector.py bash -c translate-apertium "$@"
 
