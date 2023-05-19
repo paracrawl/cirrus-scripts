2c489e33e1c297e1c5533de4a00593b2702b43fe
ZJaume <jzaragoza@prompsit.com>
2023-05-19 18:12:09 +0300

    Use foldfilter in translate-bergamot

--- models/translate-bergamot.sh
+++ models/translate-bergamot.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 MARIAN=/projappl/project_462000252/software/marian-bergamot
 MODEL=$(dirname $(realpath -es ${BASH_SOURCE[0]}))/model
 
+foldfilter -s -w 500 \
 $MARIAN/marian-decoder \
 	-c $MODEL/config.yml \
 	--cpu-threads $THREADS \
