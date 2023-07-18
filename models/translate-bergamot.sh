7821e8d4fdcc1ba9469057be4d2efccb87653297
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-07-18 14:00:58 +0300

    Fix bergamot model configuration path

--- models/translate-bergamot.sh
+++ models/translate-bergamot.sh
@@ -6,6 +6,6 @@ MODEL=$(dirname $(realpath -es ${BASH_SOURCE[0]}))/model
 
 foldfilter -s -w 500 \
 $MARIAN/marian-decoder \
-	-c $MODEL/config.yml \
+	-c $MODEL/config.intgemm8bit.alphas.yml \
 	--cpu-threads $THREADS \
 	--quiet-translation
